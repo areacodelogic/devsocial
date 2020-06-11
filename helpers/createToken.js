19337a1ca4bcd2dcdb5837e1a4293d790b623b88
timothy egart <areacodelogic@gmail.com>
2020-06-10 23:52:49 -0700

    Added user registration, implement JWT verify and auth middleware

---
+++ helpers/createToken.js
@@ -0,0 +1,17 @@
+// const jwt = require('jsonwebtoken');
+// const config = require('config');
+
+
+// function createToken(user){
+//     let payload = {
+//         user: {
+//             id: user.id
+//         }
+//     }
+
+//     let getToken = config.get('jwtToken')
+
+//     return jwt.sign(payload, getToken)
+// }
+
+// module.exports = createToken;
