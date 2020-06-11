d03f1b9a3ab8bf7d67924922fb7a48292fe84962
timothy egart <areacodelogic@gmail.com>
2020-06-11 04:25:23 -0700

    Can add experience to profile, delete experience from profile,stil working on updating the  experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -131,17 +131,162 @@ router.get('/user/:user_id', async (req, res) => {
       user: req.params.user_id,
     }).populate('user', ['name', 'avatar']);
 
-    if (!profile)
-      return res.status(400).json({ msg: 'Profile not found' });
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
 
     res.json(profile);
   } catch (err) {
     console.log(err.message);
-    if(err.kind === "ObjectId"){
+    if (err.kind === 'ObjectId') {
       return res.status(400).json({ msg: 'Profile not found' });
     }
     res.status(500).send('Server Error');
   }
 });
+
+// @route   DELETE api/profile
+// @desc    DELETE profile, user & posts
+// @access  Private
+router.delete('/', authorize, async (req, res) => {
+  try {
+    // @todo remove users posts
+
+    // Remove Profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove User
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User Deleted' });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   PUT api/profile/experience
+// @desc    ADD profile experience
+// @access  Private
+
+router.put(
+  '/experience',
+  [
+    authorize,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From Date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+
+router.delete('/experience/:exp_id', authorize, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @todo get the user to be able to update their experience
+
+// @route   PUT api/profile/experience/:exp_id
+// @desc    Edit profile experience
+// @access  Private
+
+// router.put(
+//   '/experience/:exp_id',
+//   [
+//     authorize,
+//     check('title', 'Title is required').not().isEmpty(),
+//     check('company', 'Company is required').not().isEmpty(),
+//     check('from', 'From date is required').not().isEmpty()
+//   ],
+//   async (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
+
+//     const { title, company, location, from, to, current, description } = req.body;
+//     // since title, company, from are required
+//     const exp = { title, company, from };
+//     if (location) exp.location = location;
+//     if (to) exp.to = to;
+//     if (current) exp.current = current;
+//     if (description) exp.description = description;
+
+//     try {
+//       const profile = await Profile.findOneAndUpdate(
+//         { userid: req.user.id, 'experience._id': req.params.exp_id },
+//         {
+//           $set: {
+//             // I don't want my experience id to change
+//             'experience.$': { _id: req.params.exp_id, ...exp }
+//           }
+//         },
+//         { new: true }
+//       );
+
+//       res.json(profile);
+//     } catch (err) {
+//       console.log(err.message);
+//       res.status(500).send('Server Error');
+//     }
+//   }
+// );
 
 module.exports = router;
