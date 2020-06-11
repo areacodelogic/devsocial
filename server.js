d6a6f1c8f8cf8ba1ca6f893260ed09c21f8c04e4
timothy egart <areacodelogic@gmail.com>
2020-06-10 18:30:37 -0700

    Created initial set up, config db, routes, serever

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db')
+const usersRoutes = require('./routes/api/users')
+const postsRoutes = require('./routes/api/posts')
+const authRoutes = require('./routes/api/auth')
+const profileRoutes = require('./routes/api/profile')
 
 const app = express();
 
+// Connect Database
+connectDB()
+
 app.get('/', (req, res) => res.send("API running"))
+
+// Define Routes
+app.use('/api/users', usersRoutes)
+app.use('/api/posts', postsRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/profile', profileRoutes)
 
 const PORT = process.env.PORT || 5000;
 
