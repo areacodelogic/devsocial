3d03cc8a44223c38065a31dc3c99d1694cc5b487
timothy egart <areacodelogic@gmail.com>
2020-06-12 19:17:40 -0700

    Working on custom themes with scss, finished landing, register,login

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+import './App.css';
+
+const App = () => (
+  <Fragment>
+    <h1>hi</h1>
+  </Fragment>
+);
+
+export default App;
