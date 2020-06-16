b81213a94410e8e47b1bc0342ee2026b03555bbc
timothy egart <areacodelogic@gmail.com>
2020-06-15 19:37:13 -0700

    Found bug, fixed problem, can now make request to github and receive repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -395,7 +395,7 @@ router.get('/github/:username', async (req, res) => {
     );
     const headers = {
       'user-agent': 'node.js',
-      Authorization: `token ${config.get('githubToken')}`
+      Authorization: `token${config.get('githubToken')}`
     };
 
     
